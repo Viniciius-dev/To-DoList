a0a13d27183e765b09d642de29fcf0f511431189
Vinícius-Castro <viniciuscasalves654@gmail.com>
2026-08-20 20:20:52 -0300

    revendo erros

--- src/main/java/org/example/todolist/entity/Todo.java
+++ src/main/java/org/example/todolist/entity/Todo.java
@@ -1,12 +1,16 @@
 package org.example.todolist.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
 @Table(name = "todos")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,4 +19,11 @@ public class Todo {
     private String descricao;
     private boolean realizado;
     private int prioridade;
+
+    public Todo(String nome, String descricao, boolean realizado, int prioridade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.realizado = realizado;
+        this.prioridade = prioridade;
+    }
 }
--- src/test/java/org/example/todolist/ToDoListApplicationTests.java
+++ src/test/java/org/example/todolist/ToDoListApplicationTests.java
@@ -1,13 +1,60 @@
 package org.example.todolist;
 
+import org.example.todolist.entity.Todo;
+import org.example.todolist.repository.TodoRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ToDoListApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @BeforeEach
+    void setUp() {
+        todoRepository.deleteAll();
     }
 
+    @Test
+    void testCreateTodoSucess() throws Exception {
+        var todo = new Todo("todo 1", "desc todo 1", false, 1);
+
+        mockMvc.perform(post("/todos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(todo)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$[0].nome").value("todo 1"))
+                .andExpect(jsonPath("$[0].descricao").value("desc todo 1"))
+                .andExpect(jsonPath("$[0].realizado").value(false))
+                .andExpect(jsonPath("$[0].prioridade").value(1));
+    }
+
+    @Test
+    void testCreateTodoFail() throws Exception {
+        var todo = new Todo("", "", false, 0);
+
+        mockMvc.perform(post("/todos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(todo)))
+                .andExpect(status().isOk());
+    }
 }
